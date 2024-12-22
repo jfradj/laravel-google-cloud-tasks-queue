69860bb22bad55ac8c7a525b28de7fa4eaa28cf4
Johann Fradj <fradj.johann@gmail.com>
2024-12-22 16:51:01 +0100

    Updating Worker to fix SIGALRM issues

--- src/Worker.php
+++ src/Worker.php
@@ -6,6 +6,7 @@ namespace Stackkit\LaravelGoogleCloudTasksQueue;
 
 use Illuminate\Queue\Worker as LaravelWorker;
 use Illuminate\Queue\WorkerOptions;
+use Throwable;
 
 /**
  * Custom worker class to handle specific requirements for Google Cloud Tasks.
@@ -25,6 +26,63 @@ use Illuminate\Queue\WorkerOptions;
  */
 class Worker extends LaravelWorker
 {
+    public function daemon($connectionName, $queue, WorkerOptions $options): int
+    {
+        if ($supportsAsyncSignals = $this->supportsAsyncSignals()) {
+            $this->listenForSignals();
+        }
+
+        if (isset($this->resetScope)) {
+            ($this->resetScope)();
+        }
+
+        // First, we will attempt to get the next job off of the queue. We will also
+        // register the timeout handler and reset the alarm for this job so it is
+        // not stuck in a frozen state forever. Then, we can fire off this job.
+        $job = $this->getNextJob(
+            $this->manager->connection($connectionName), $queue
+        );
+
+        if ($supportsAsyncSignals) {
+            $this->registerTimeoutHandler($job, $options);
+        }
+
+        // If the daemon should run (not in maintenance mode, etc.), then we can run
+        // fire off this job for processing. Otherwise, we will need to sleep the
+        // worker so no more jobs are processed until they should be processed.
+        if ($job) {
+            $this->runJob($job, $connectionName, $options);
+
+            if ($options->rest > 0) {
+                $this->sleep($options->rest);
+            }
+        }
+
+        if ($supportsAsyncSignals) {
+            $this->resetTimeoutHandler();
+        }
+
+        return static::EXIT_SUCCESS;
+    }
+
+    protected function daemonShouldRun(WorkerOptions $options, $connectionName, $queue): bool
+    {
+        return ! (($this->isDownForMaintenance)() && ! $options->force);
+    }
+
+    protected function pauseWorker(WorkerOptions $options, $lastRestart)
+    {
+        // Nothing to do
+    }
+
+    /**
+     * @throws Throwable
+     */
+    protected function runJob($job, $connectionName, WorkerOptions $options): void
+    {
+        $this->process($connectionName, $job, $options);
+    }
+
     public function process($connectionName, $job, WorkerOptions $options): void
     {
         if ($this->supportsAsyncSignals()) {
@@ -34,18 +92,5 @@ class Worker extends LaravelWorker
         }
 
         parent::process($connectionName, $job, $options);
-    }
-
-    public function kill($status = 0, $options = null): void
-    {
-        parent::stop($status, $options);
-
-        // When running tests, we cannot run exit because it will kill the PHPunit process.
-        // So, to still test that the application has exited, we will simply rely on the
-        // WorkerStopped event that is fired when the worker is stopped.
-        if (! app()->runningUnitTests()) {
-            exit($status);
-        }
-
     }
 }
